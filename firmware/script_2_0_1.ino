9d6d545d85eb87476ccc1bdf8a9fcc4bd4922fb8
Giudev.eth <95681714+GiuBlockchainDEV@users.noreply.github.com>
2025-02-10 20:09:45 +0300

    Update script_2_0_1.ino

--- firmware/script_2_0_1.ino
+++ firmware/script_2_0_1.ino
@@ -6,8 +6,8 @@
 #define FIRMWARE_VERSION "1.0.5"
 #define MAX_UPDATE_ATTEMPTS 3
 
-const char* ssid = "Growa";
-const char* password = "Montblanc89!";
+const char* ssid = "";
+const char* password = "";
 const char* version_url = "https://raw.githubusercontent.com/Growa-AI/OTA/main/firmware/version.txt";
 const char* firmware_url = "https://raw.githubusercontent.com/Growa-AI/OTA/main/firmware/firmware.ino.bin";
 
